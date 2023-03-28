a1a9621294151dd933bab343d9e406ffd4e30345
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-03-28 23:43:42 +0300

    Отправка на проверку

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from posts.models import Post, Group, Comment
+from posts.models import Post, Group
 from rest_framework import viewsets
 from rest_framework.exceptions import PermissionDenied
 
